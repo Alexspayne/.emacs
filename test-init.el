e1f2bf76150d8a52a2b4a9aee2d6fc0235464e9f
Alex <apayne@elizacorp.com>
2017-03-07 13:13:17 -0500

    Remove more non-config files

--- test-init.el
+++
@@ -1,8 +0,0 @@
-;; Test init file to run bits of configuration in isolation.
-;; Run with `'emacs -Q -l ~/.emacs.d/test-init.el`'
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-(global-auto-complete-mode t)
