5a9825fb4199235773979e18844901e739e5ecfc
Alex <apayne@elizacorp.com>
2017-03-07 13:15:59 -0500

    Remove a lot of unused configurations

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
 ;; Load configuration from ~/.emacs.d/settings.org
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(setq load-prefer-newer t)
+(setq user-emacs-directory-warning nil)
 (require 'org)
 (org-babel-load-file
     (expand-file-name "settings.org"
